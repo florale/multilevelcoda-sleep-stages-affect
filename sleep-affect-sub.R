781dcac58dd087724773dd86d4716c2ffd8d0144
Flora Le <flora.le@monash.edu>
2024-04-04 23:12:39 +1100

    more update analysis using new multilevelcoda version

--- sleep-affect-sub.R
+++ sleep-affect-sub.R
@@ -6,32 +6,38 @@ m_lapa <- readRDS(paste0(outputdir, "m_lapa", ".RDS"))
 m_hana <- readRDS(paste0(outputdir, "m_hana", ".RDS"))
 m_lana <- readRDS(paste0(outputdir, "m_lana", ".RDS"))
 
-plan(multisession, workers = 5)
 m_hapa_sub_adj <- substitution(
   m_hapa,
   delta = c(1:60),
   # delta = 30,
   level = c("between", "within"),
-  ref = "grandmean")
+  ref = "grandmean",
+  cores = 5
+)
 m_lapa_sub_adj <- substitution(
   m_lapa,
   delta = c(1:60),
   # delta = 30,
   level = c("between", "within"),
-  ref = "grandmean")
+  ref = "grandmean",
+  cores = 5
+)
 m_hana_sub_adj <- substitution(
   m_hana,
   delta = c(1:60),
   # delta = 30,
   level = c("between", "within"),
-  ref = "grandmean")
+  ref = "grandmean",
+  cores = 5
+)
 m_lana_sub_adj <- substitution(
   m_lana,
   delta = c(1:60),
   # delta = 30,
   level = c("between", "within"),
-  ref = "grandmean")
-plan(sequential)
+  ref = "grandmean",
+  cores = 5
+)
 
 summary(m_hapa_sub_adj, delta = 30)
 summary(m_lapa_sub_adj, delta = 30)
