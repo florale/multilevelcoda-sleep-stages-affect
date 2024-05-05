c11fc3287643f636ccef675ead87a60ccf6b7973
Flora Le <flora.le@monash.edu>
2024-05-05 20:37:02 +1000

    standardised results

--- sleep-affect-sub.R
+++ sleep-affect-sub.R
@@ -48,3 +48,42 @@ saveRDS(m_hapa_sub_adj, paste0(outputdir, "m_hapa_sub_adj", ".RDS"))
 saveRDS(m_lapa_sub_adj, paste0(outputdir, "m_lapa_sub_adj", ".RDS"))
 saveRDS(m_hana_sub_adj, paste0(outputdir, "m_hana_sub_adj", ".RDS"))
 saveRDS(m_lana_sub_adj, paste0(outputdir, "m_lana_sub_adj", ".RDS"))
+
+
+# standardised coefs ------------------
+m_hapa_sub_adj <- readRDS(paste0(outputdir, "m_hapa_sub_adj", ".RDS"))
+m_lapa_sub_adj <- readRDS(paste0(outputdir, "m_lapa_sub_adj", ".RDS"))
+m_hana_sub_adj <- readRDS(paste0(outputdir, "m_hana_sub_adj", ".RDS"))
+m_lana_sub_adj <- readRDS(paste0(outputdir, "m_lana_sub_adj", ".RDS"))
+
+
+egltable(c(
+  "PosAffHALead", "PosAffLALead", "NegAffHALead", "NegAffLALead",
+  "BPosAffHALead", "BPosAffLALead", "BNegAffHALead", "BNegAffLALead",
+  "WPosAffHALead", "WPosAffLALead", "WNegAffHALead", "WNegAffLALead"
+),
+idvar = "ID",
+data = d[Survey == "Wake"][, (fvars) := lapply(.SD, as.factor), .SDcols = fvars]
+)
+
+d_sub_hapa <- as.data.table(summary(m_hapa_sub_adj, delta = 30, digits = "asis"))
+d_sub_lapa <- as.data.table(summary(m_lapa_sub_adj, delta = 30, digits = "asis"))
+d_sub_hana <- as.data.table(summary(m_hana_sub_adj, delta = 30, digits = "asis"))
+d_sub_lana <- as.data.table(summary(m_lana_sub_adj, delta = 30, digits = "asis"))
+
+d_sub_hapa[Level == "between", SMean := round(Mean / sd(d[Survey == "Wake"]$BPosAffHALead, na.rm = T), 2)]
+d_sub_hapa[Level == "within", SMean := round(Mean / sd(d[Survey == "Wake"]$WPosAffHALead, na.rm = T), 2)]
+
+d_sub_lapa[Level == "between", SMean := round(Mean / sd(d[Survey == "Wake"]$BPosAffLALead, na.rm = T), 2)]
+d_sub_lapa[Level == "within", SMean := round(Mean / sd(d[Survey == "Wake"]$WPosAffLALead, na.rm = T), 2)]
+
+d_sub_hana[Level == "between", SMean := round(Mean / sd(d[Survey == "Wake"]$BNegAffHALead, na.rm = T), 2)]
+d_sub_hana[Level == "within", SMean := round(Mean / sd(d[Survey == "Wake"]$WNegAffHALead, na.rm = T), 2)]
+
+d_sub_lana[Level == "between", SMean := round(Mean / sd(d[Survey == "Wake"]$BNegAffLALead, na.rm = T), 2)]
+d_sub_lana[Level == "within", SMean := round(Mean / sd(d[Survey == "Wake"]$WNegAffLALead, na.rm = T), 2)]
+
+d_sub_hapa
+d_sub_lapa
+d_sub_hana
+d_sub_lana
